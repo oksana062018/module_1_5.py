33b2b8ca24f436ebf6415ab7c17499c380630fb0
oksana062018 <natina.o@mail.ru>
2024-08-23 14:04:17 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = ('Elephant','Buffalo', 'Galagos', 'Baboon', 'Zebra', 'Lion', 'Rhinoceros', 'Gorilla', 'Bongo', 'Hyena', 'Cheetah', 1, 2, 3, 4, 5)
+print(immutable_var)
+mutable_list = ['понедельник','вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье', 1,2,3,4,5,6,]
+mutable_list[0] = 'январь'
+print(mutable_list)
+mutable_list.append('week')
+print(mutable_list)
